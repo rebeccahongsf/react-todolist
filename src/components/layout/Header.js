cb77ce4f531d53e3374ccb4ae0860768933c0605
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-20 20:14:12 -0800

    Update Header links and About page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header style={headerStyle}>
       <h1>Todo List</h1>
-      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+      <Link style={linkStyle} to="/react-todolist/">Home</Link> | <Link style={linkStyle} to="/react-todolist/about">About</Link>
     </header>
   )
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,10 @@ import React from 'react'
 function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
+      <h2>About</h2>
       <p> This is the Todolist app v1.0.0</p>
+      <h3>How do use this Todo List</h3>
+      <p>Add a Todo task within the Todo text box.<br/>Cross-out the completed task by clicking on the text.<br/>Delete the task by clicking the DEL button located to the right.</p>
     </React.Fragment>
   )
 }
